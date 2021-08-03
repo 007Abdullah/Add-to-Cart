a62fecbd919ba2249f5c3c597e0a778ba93b3f72
Muhammad Abdullah <kb337137@gmail.com>
2021-08-03 07:04:25 -0700

    show data with useContext

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,42 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { products } from './../products';
 import './cart.css';
+import Contextdata from './Contextdata';
+
+export const CartContext = createContext();
 
 const Cart = () => {
+
     return (
         <React.Fragment>
-            <header>
-                <div style={{ width: '80%', display: 'flex', justifyContent: 'space-between', borderBottom: '1px solid black', margin: '0 auto', position: 'relative' }}>
-                    <div className="header">
-                        <img src="./images/backarrow.png" alt="back" />
-                        <h3 style={{ marginTop: '35px' }}>Continue Shopping</h3>
-                    </div>
-                    <div className="second-main">
-                        <img src="./images/backet.png" alt="backet" style={{ width: '80px' }} />
-                        <p className="paraStyle">7</p>
-                    </div>
-                </div>
-            </header>
-            <section className="main-cart-section">
-                <div style={{ width: '100%', marginBottom: '30px' }}>
-                    <h1>Shopping Cart</h1>
-                    <br />
-                    <p>You Have <span><b>4</b></span> Items In Shopping Cart</p>
-                </div>
-                <div className="cart-Item">
-                    <div className="center-scroll-part">
-                        {/* <Scrollbars>
-                            {Object.keys(getproduct).map((eacthItem) => {
-                                const product = getproduct[eacthItem]
-                                return <CartItem product={product} />
-                            })}
-                        </Scrollbars>    */}
-                    </div>
-                </div>
-                <div style={{ width: '80%', margin: '0 auto', textAlign: 'end' }}>
-                    <h3>Cart Total : <span>2200$</span></h3>
-                    <button style={{ margin: '10px', padding: '10px', backgroundColor: 'lightblue', color: 'white', borderRadius: '10px' }}>Checkout</button>
-                </div>
-            </section>
+            <CartContext.Provider value={products}>
+                <Contextdata />
+            </CartContext.Provider>
         </React.Fragment>
     )
 }
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Items = () => {
+const Items = ({ id, title, description, price, img, amount }) => {
+    console.log(' ITEM id :', title);
     return (
         <React.Fragment>
-
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
+                <div style={{ width: '20%' }}>
+                    <img src={img} alt={title} style={{ width: '100px' }} />
+                </div>
+                <div style={{ width: '20%' }}>
+                    <div style={{ margin: '20px' }}>
+                        <h4>{title}</h4>
+                        <p>{description}</p>
+                    </div>
+                </div>
+                <div style={{ width: '20%' }}>
+                    <div style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '25px' }}>
+                        <img src="./images/minus.png" alt="minus" />
+                        <input type="text" placeholder="2" style={{ width: '20px', textAlign: 'center' }} />
+                        <img src="./images/plus.png" alt="plus" />
+                    </div>
+                </div>
+                <div style={{ width: '20%' }}>
+                    <h1 style={{ textAlign: 'center', marginTop: '23px' }}>{price}</h1>
+                </div>
+                <div style={{ width: '20%', textAlign: 'center' }}>
+                    <img src="./images/close.png" alt="close" style={{ marginTop: '20px' }} />
+                </div>
+            </div>
         </React.Fragment>
     )
 }
